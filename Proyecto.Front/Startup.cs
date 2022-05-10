1b6a4097143ad3f9fad243043aabd47a64c0deeb
Salvador Hernández Galindo <mexboomer@outlook.com>
2022-05-09 21:54:25 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto.Front/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto.Front.Startup))]
+namespace Proyecto.Front
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
